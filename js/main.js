03b4859b43024a5ff031fb43e0c0182b2ec2ceda
Alx265 <marcin265@onet.eu>
2021-07-29 00:29:15 +0200

    toTop btn

--- js/main.js
+++ js/main.js
@@ -11,3 +11,14 @@ window.addEventListener("scroll",() => {
 	const navigation = document.querySelector(".main-nav");
 	navigation.classList.toggle("sticky",window.scrollY > 100);
 })
+
+  
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+	if(window.pageYOffset > 100) {
+		toTop.classList.add("active");
+	} else {
+		toTop.classList.remove("active");
+	}
+});
