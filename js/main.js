13ca5ea2d3bea176b77b4fba66d62c7ac333935e
Alx265 <marcin265@onet.eu>
2021-07-29 23:22:30 +0200

    Js fix

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
 
 
+
 const navSlide = () => {
   burger.addEventListener('click' , () => {
     nav.classList.toggle('nav-active');
@@ -51,11 +52,9 @@ const navClose = () => {
   nav.addEventListener('click' , () => {
     nav.classList.remove('nav-active');
     burger.classList.remove('toggle');
-		navLinks.forEach((link,index) => {
+		navLinks.forEach((link) => {
       if(link.style.animation){
         link.style.animation = '';
-      } else {
-        link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
       }
     });
   });
