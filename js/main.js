57aa483f9058b3d12d0f58a923ccb22354e9c0f3
Alx265 <marcin265@onet.eu>
2021-07-29 02:21:42 +0200

    Scroll reveal

--- js/main.js
+++ js/main.js
@@ -22,3 +22,36 @@ window.addEventListener("scroll", () => {
 		toTop.classList.remove("active");
 	}
 });
+
+
+
+// Animations
+const sr = ScrollReveal({
+	distance: '300px',
+	duration: 1500,
+	reset: true
+})
+
+// Header-content
+sr.reveal('.text-hello', {origin:'left'});
+sr.reveal('.text-name', {origin:'right', delay: 800, scale: 0.5});
+sr.reveal('.text-frontend', {origin:'right', delay: 1600});
+sr.reveal('.social', {origin:'left', delay: 2000});
+sr.reveal('#header-social', {origin:'left', delay: 2000});
+
+// About
+sr.reveal('.about-me', {origin:'top'});
+sr.reveal('.about-photo', {origin:'left',delay: 800});
+sr.reveal('.about-text', {origin:'right',delay: 800});
+
+// Skills
+sr.reveal('.skills', {origin:'top'});;
+sr.reveal('.items-container', {origin:'right',delay: 800});
+
+// Portfolio 
+sr.reveal('.portfolio', {origin:'top'});;
+sr.reveal('.portfolio-container', {origin:'bottom',delay: 800});
+
+
+
+
